fed9622f811255d6032ec8b27a2b1895726283ce
chana-vander <c89408@gmail.com>
2025-03-11 19:11:18 +0200

    final!

--- Courses-client/src/app/app.component.ts
+++ Courses-client/src/app/app.component.ts
@@ -1,16 +1,23 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterModule } from '@angular/router'; // ייבוא RouterOutlet ו-RouterModule
 import { HomeComponent } from '../components/home/home.component';
-// ייבוא המודולים של Angular Material
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatCardModule } from '@angular/material/card';
-import { RouterModule } from '@angular/router';
+import { ShortenPipe } from '../pipes/shorten.pipe'; // ייבוא ShortenPipe
 @Component({
   selector: 'app-root',
-  imports: [HomeComponent,RouterModule,MatButtonModule,MatToolbarModule,MatCardModule],
+  imports: [
+    HomeComponent,
+    RouterModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatCardModule,
+    RouterOutlet, // הוספת RouterOutlet ל-imports
+    ShortenPipe, // הוספת ShortenPipe ל-imports (אם משתמשים בו)
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'courses';
